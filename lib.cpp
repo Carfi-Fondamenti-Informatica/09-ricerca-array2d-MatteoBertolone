2d4955e46dc845322fdd9a06d449cc40da59e9ac
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-13 22:57:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
 
+int comp(char list[10][20], char name[]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(list[i][j] != name[j])
+                break;
+            else{
+                if(j==19){
+                    return i+1;
+                }
+            }
+        }
+    }
+    return -1;
+}
