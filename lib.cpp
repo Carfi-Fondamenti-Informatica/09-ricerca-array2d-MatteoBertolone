61ffd1a8eda6ae5bf15af25555a30ea02d6bee42
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-14 16:54:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,13 @@
 #include "lib.h"
 
-bool comp(char list[10][20], int &x, char name[]){
+bool comp(char list[10][20], int &p, char name[]){
     for(int i=0;i<10;i++){
         for(int j=0;j<20;j++){
             if(list[i][j] != name[j]){
                 j = 20;
             }else{
               if(j==19){
-                  x = i;
+                  p = i;
                   return true;
               }
             }
